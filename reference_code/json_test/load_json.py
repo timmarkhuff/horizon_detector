606f04ed86bb711a73426591225444616154e2ab
timmarkhuff <89954856+timmarkhuff@users.noreply.github.com>
2022-11-09 17:05:23 -0800

    Delete reference_code directory

--- reference_code/json_test/load_json.py
+++
@@ -1,7 +0,0 @@
-import json
-
-# Opening JSON file
-with open('test_json.txt') as json_file:
-    datadict = json.load(json_file)
-
-print(datadict['metadata']['fps'])
--- reference_code/picamera_test.py
+++
@@ -1,6 +0,0 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
-
-camera = PiCamera()
